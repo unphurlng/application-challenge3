3f933992a5a993177a6c6d172f466add39638199
Heather <unphurlng@gmail.com>
2019-02-08 16:05:10 -0800

    radio button conditionals

--- src/apply/apply.js
+++ src/apply/apply.js
@@ -1,12 +1,16 @@
 const applicationFormNode = document.getElementById('application-form'); //1
+const nameNode = document.getElementById('name'); //3
+const emailNode = document.getElementById('email'); //3
+const phoneNode = document.getElementById('phone'); //3
 const fullTimeNode = document.getElementById('full-time'); //5
 const partTimeNode = document.getElementById('part-time'); //5
+const positionsNode = document.getElementById('positions'); //7
 
 // file - apply.js
     // 1 globals - grab form by id
     // 2 add event listener and preventDefault
         // a. check to make sure that apply.js is hooked up and preventDefault is working
-    // 3 grab value of nameNode element within event listener - assign to variable
+    // 3 globals - grab value of nameNode element 
     // 3a console.log
     // 3b repeat for email and phone
     // 4 create for loop for check boxes
@@ -16,31 +20,19 @@ const partTimeNode = document.getElementById('part-time'); //5
         // d. conditional - if statement - if gender.checked then push gender to empty array
         // e. console.log
     // 5 globals - grab nodes for radio buttons
-    // 6 create event listeners for radio buttons (one for each one) - above larger event listener
-        // a. assign node.value to iput name (schedule)
-        // b. conditional - if statement - if node.checked then schedule = node.value 
-    // 7 grab positions value inside event listener
+    // 6 need conditional for radio buttons within event listener
+        // a. let schedule = null;
+        // b. conditional for each button - if node.checked then schedule = node.value 
+    // 7 globals - grab node for pull-down
     // 8 create applicant object inside event listener
+    // 9 stringify object - assign to variable
+    // 10 put stringified object into local storage
+// file - applicant-detail.js
 
-let schedule = fullTimeNode.value; //6a
-fullTimeNode.addEventListener('change', function() { //6
-    if(fullTimeNode.checked) { //6b
-        schedule = fullTimeNode.value;
-    }
-});
-partTimeNode.addEventListener('change', function() { //6
-    if(partTimeNode.checked) { //6b
-        schedule = partTimeNode.value;
-    }
-});
 
-applicationFormNode.addEventListener('submit', function(event) {
+
+document.addEventListener('submit', function(event) {
     event.preventDefault();
-
-    const nameNode = applicationFormNode.elements.name.value; //3
-    const emailNode = applicationFormNode.elements.email.value; //3
-    const phoneNode = applicationFormNode.elements.phone.value; //3
-    const positionsNode = applicationFormNode.elements.positions.value; //7
 
     let genderChecked = []; //4a
     for(let i = 0; i < applicationFormNode.gender.length; i++) { //4b
@@ -48,7 +40,27 @@ applicationFormNode.addEventListener('submit', function(event) {
         if(gender.checked) { //4d
             genderChecked.push(gender.value);
         }
+        console.log('what gender', genderChecked);
     }
 
+    let schedule = null; //6a
+    if(fullTimeNode.checked) { //6b
+        schedule = fullTimeNode.value;
+    }
+    if(partTimeNode.checked) {
+        schedule = partTimeNode.value;
+    }
+
+    const applicantObject = { //8
+        nameKey: nameNode.value,
+        emailKey: emailNode.value,
+        phoneKey: phoneNode.value,
+        genderKey: genderChecked.value,
+        scheduleKey: schedule,
+        positionsKey: positionsNode.value
+    };
+
+    const applicantObjectString = JSON.stringify(applicantObject); //9
+    window.localStorage.setItem('applicantObject', applicantObjectString); //10
 
 });
